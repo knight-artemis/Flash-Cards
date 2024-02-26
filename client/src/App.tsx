05d4dcaeaff0e631427c7bdb1727a234481fb04b
Nikolai Kapitonov <Koluankapitonov@mail.ru>
2024-02-26 11:41:02 +0300

    kolya routers App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+
 function App() {
   return (
-    <div className='main'>
-      Тут будут роуты
+    <div className="main">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/game" element={<GamePage />} />
+        <Route path="/lk" element={<LkPage />} />
+        <Route path="/game" element={<GamePage />} />
+        <Route path="/endgame" element={<EndPage />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
