56544e40203b942595877e1efa71c1a4f58f2b0c
JuckieChan <luksor144@gmail.com>
2024-02-26 18:57:32 +0400

    auth modded

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,27 @@
-import Header from "./components/Header/Header";
-import Auth from "./components/Auth/Auth";
+import Header from './components/Header/Header';
+import Auth from './components/Auth/Auth';
 
-import "./App.css";
-import HomePage from "./pages/HomePage/HomePage";
-import { Route, Routes } from "react-router-dom";
-import LkPage from "./pages/LkPage/LkPage";
-import GamePage from "./pages/GamePage/GamePage";
-import EndGamePage from "./pages/EndGamePage/EndGamePage";
+import './App.css';
+import HomePage from './pages/HomePage/HomePage';
+import { Route, Routes } from 'react-router-dom';
+import LkPage from './pages/LkPage/LkPage';
+import GamePage from './pages/GamePage/GamePage';
+import EndGamePage from './pages/EndGamePage/EndGamePage';
+import { useAppDispatch } from './redux/hooks';
+import { useEffect } from 'react';
+import axios from 'axios';
+import Actions from './redux/actions';
 
 function App() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_URL}/auth/`, { withCredentials: true })
+      .then((res) => dispatch(Actions.checkAuth(res.data)))
+      .catch((err) => console.log(err));
+  }, [dispatch]);
+
   return (
     <div className="main">
       <Header />
--- client/src/redux/userReducer.ts
+++ client/src/redux/userReducer.ts
@@ -1,25 +1,28 @@
-import type { UserType } from "../types";
+import type { UserType } from '../types';
 
 const initialState: UserType = {
   userId: 0,
-  login: ''
+  login: '',
 };
 
 export type UserActionType = {
-  type: string,
-  payload: UserType
-}
+  type: string;
+  payload: UserType;
+};
 
 // eslint-disable-next-line @typescript-eslint/default-param-last
 const userReducer = (state = initialState, action: UserActionType): UserType => {
   const { type, payload } = action;
 
   switch (type) {
-    case 'REG_LOG': 
-      return payload
+    case 'REG_LOG':
+      return payload;
+
+    case 'CHECK_AUTH':
+      return payload;
 
     case 'LOGOUT':
-      return initialState
+      return initialState;
 
     default:
       return state;
