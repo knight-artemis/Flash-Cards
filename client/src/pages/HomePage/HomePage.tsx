0cc00ee2539eab16459cfb1ca396d812c97bb13b
manGo0407 <gusejnov.roman@bk.ru>
2024-02-27 12:31:40 +0300

    homepage

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -1,14 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import style from "./HomePage.module.css";
 
 export default function HomePage() {
   return (
-    <div className='home-page'>
+    <div className="home-page">
       <h2>ДОБРО ПОЖАЛОВАТЬ НА НАШ САЙТ</h2>
       <h2>Перед началом зарегистрируйтесь или войдите!</h2>
-      <h2>ПРАВИЛА ИГРЫ ОПИСАТЬ ТУТ</h2>
+      <div className={style.rulesContainer}>
+        <h2>ПРАВИЛА ИГРЫ:</h2>
+        <div className="onlyRules">
+          <h4>1. Есть один игрок и не поверите, это-Вы!</h4>
+          <h4>
+            2. Есть 5 тем, каждая из которых содержит 5 вопросов разной
+            сложности.
+          </h4>
+          <h4>
+            3. Чем сложнее вопрос, тем больше очков вы получите за правильный
+            ответ.
+          </h4>
+          <h4>
+            4. Если вы ответили неверно, у игрока будет вычитаться половина
+            стоимости данного вопроса.
+          </h4>
+          <h4>
+            5. Ты можешь завершить игру не отвечая на все имеющиеся вопросы!
+          </h4>
+          <h4>6. Удачной игры!</h4>
+        </div>
+      </div>
       <p>
-        Если зарегистрирован: <Link to='/game'><button>Начать игру</button></Link>
+        Если зарегистрирован:{" "}
+        <Link to="/game">
+          <button className={style.gameButton}><b>Начать игру</b></button>
+        </Link>
       </p>
     </div>
   );
