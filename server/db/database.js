1a4e88af3cb71d16dcc1d95e1bca35d8b54481cb
Artem Sobakar <sobakar.artyom@gmail.com>
2024-03-25 13:03:04 +0300

    readme upd

--- server/db/database.js
+++ server/db/database.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   development: {
     username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: process.env.USER_PASSWORD,
     database: process.env.DB_NAME,
     host: '127.0.0.1',
     dialect: 'postgres',
